9099c075ba2621c199c19bdb8c16fb9355f5386e
Đức Hoàng <136428671+hoangvhvh@users.noreply.github.com>
2024-01-03 09:50:45 +0700

    Add files via upload

--- HomeActivity.java
+++ HomeActivity.java
@@ -6,28 +6,40 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import adapter.ConstantAdapter;
 import org.coolstyles.demologin.util.Constants;
 
 public class HomeActivity extends AppCompatActivity {
     private RecyclerView rvContact;
+    private Button showAlertDialogButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        Intent intent = getIntent();
-        intent.getIntExtra(Constants.PRODUCT_ID, 1);
-    }
-
-    private void initGUI(){
-        String dataList[] = {"DucHoang", "HuuLoi", "SyHung", "VanChinh"};
-
+        showAlertDialogButton = findViewById(R.id.showAlertDialogButton);
         rvContact = findViewById(R.id.rv_contact);
-        rvContact.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false));
-        ConstantAdapter adapter = new ConstantAdapter(dataList);
-        rvContact.setAdapter(adapter);
+
+      //  initGUI();
+
+        showAlertDialogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, CustomDialogActivity.class);
+                AlertDialogExample.showAlertDialog(HomeActivity.this, intent);
+            }
+        });
     }
+
+   // private void initGUI() {
+    //    String dataList[] = {"DucHoang", "HuuLoi", "SyHung", "VanChinh"};
+
+    //    rvContact.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+    //    ConstantAdapter adapter = new ConstantAdapter(dataList);
+     //   rvContact.setAdapter(adapter);
+  //  }
 }
